f4d52d0d5734cbd394f3fbe672f788703c8c7f98
gasolin <gasolin@gmail.com>
2015-11-03 23:04:58 +0800

    remove update.webapp and update manifest.json format

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('default', function() {
-  return gulp.src(['index.js', 'manifest.json', 'update.webapp', 'LICENSE', '**/icons/*'])
+  return gulp.src(['index.js', 'manifest.json', 'LICENSE.md', '**/icons/*'])
     .pipe(zip('package.zip'))
     .pipe(gulp.dest('./'));
 });
